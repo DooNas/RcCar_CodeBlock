b919a8ed08966be4c2a1e342319e53e20d10d3a7
DooNas <jaejoon4929@gmail.com>
2022-08-17 14:02:10 +0900

    방향키 통신
    press와 down 방식 차이 찾아두기

---
+++ RcCar_CodeBlock/RcCar_CodeBlock/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace RcCar_CodeBlock
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_console = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb_console
+            // 
+            this.lb_console.AutoSize = true;
+            this.lb_console.Font = new System.Drawing.Font("맑은 고딕", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lb_console.Location = new System.Drawing.Point(221, 86);
+            this.lb_console.Name = "lb_console";
+            this.lb_console.Size = new System.Drawing.Size(66, 28);
+            this.lb_console.TabIndex = 0;
+            this.lb_console.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 210);
+            this.Controls.Add(this.lb_console);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lb_console;
+    }
+}
